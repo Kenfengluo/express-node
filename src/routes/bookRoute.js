30bb89deb0cfcf0a92767546013d816c5cf85dd6
Ken Luo <LUOK1@airproducts.com>
2018-10-15 11:45:30 -0400

    authenication done

--- src/routes/bookRoute.js
+++ src/routes/bookRoute.js
@@ -5,6 +5,13 @@ const debug = require('debug')('app:bookRoute');
 const bookRouter = express.Router();
 let books = [];
 function router(nav) {
+  bookRouter.use((req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  });
   bookRouter.route('/').get((req, res) => {
     const url = 'mongodb://localhost:27017';
     const dbname = 'libraryApp';
